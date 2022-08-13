296631e920dfff129224d124c2a97fd97543aa48
Yu-Han Luo <yuhanluo1994@gmail.com>
2022-08-13 16:28:14 +0800

    feat: add demo for playlist

--- example/lib/playlist_player.dart
+++ example/lib/playlist_player.dart
@@ -15,59 +15,110 @@ class _PlaylistPlayerState extends State<PlaylistPlayer> {
   );
   late final WebviewtubeController webviewtubeController;
   bool _isPlaying = false;
+  int _currentIdx = 0;
+  final List<String> videoIds = <String>[
+    'l6hw4o6_Wcs',
+    'jckqXR5CrPI',
+    'RA-vLF_vnng',
+  ];
 
   @override
   void initState() {
     super.initState();
-    // load the playlist right after
+    // or to load by playlist id
+    // webviewtubeController = WebviewtubeController(
+    //   onPlayerReady: () => webviewtubeController.loadPlaylist(
+    //       playlistId: 'PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG'),
+    // );
     webviewtubeController = WebviewtubeController(
-      onPlayerReady: () => webviewtubeController.loadPlaylist(
-          playlistId: 'PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG'),
+      onPlayerReady: () =>
+          webviewtubeController.loadPlaylist(videoIds: videoIds),
     );
+    webviewtubeController.addListener(_valueHandler);
+  }
+
+  @override
+  void dispose() {
+    webviewtubeController.removeListener(_valueHandler);
+    webviewtubeController.dispose();
+    super.dispose();
+  }
+
+  void _valueHandler() {
+    final playerState = webviewtubeController.value.playerState;
+    final metadata = webviewtubeController.value.videoMetadata;
+    final index = videoIds.indexWhere((e) => e == metadata.videoId);
+    if (_isPlaying && playerState != PlayerState.playing) {
+      setState(() {
+        _isPlaying = false;
+      });
+    } else if (!_isPlaying && playerState == PlayerState.playing) {
+      setState(() {
+        _isPlaying = true;
+      });
+    }
+
+    if (index > 0) {
+      setState(() {
+        _currentIdx = index;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Customized Player'),
+        title: const Text('Playlist Player'),
       ),
       body: ListView(
         children: <Widget>[
           WebviewtubePlayer(
-            videoId: '4AoFA19gbLo',
+            videoId: '',
             options: options,
             controller: webviewtubeController,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              Text(
+                'Currently playing video index: ',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              DropdownButton<int>(
+                  value: _currentIdx,
+                  items: List<DropdownMenuItem<int>>.generate(videoIds.length,
+                      (i) {
+                    return DropdownMenuItem<int>(
+                      value: i,
+                      child: Text(i.toString()),
+                    );
+                  }),
+                  onChanged: (index) {
+                    if (index == null) return;
+                    webviewtubeController.playVideoAt(index);
+                  }),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
               IconButton(
-                  onPressed: () {
-                    webviewtubeController.previousVideo();
-                  },
+                  onPressed: _currentIdx > 0
+                      ? () => webviewtubeController.previousVideo()
+                      : null,
                   icon: const Icon(Icons.skip_previous)),
               _isPlaying
                   ? IconButton(
-                      onPressed: () {
-                        webviewtubeController.pause();
-                        setState(() {
-                          _isPlaying = false;
-                        });
-                      },
+                      onPressed: () => webviewtubeController.pause(),
                       icon: const Icon(Icons.pause))
                   : IconButton(
-                      onPressed: () {
-                        webviewtubeController.play();
-                        setState(() {
-                          _isPlaying = true;
-                        });
-                      },
+                      onPressed: () => webviewtubeController.play(),
                       icon: const Icon(Icons.play_arrow)),
               IconButton(
-                  onPressed: () {
-                    webviewtubeController.nextVideo();
-                  },
+                  onPressed: _currentIdx < videoIds.length - 1
+                      ? () => webviewtubeController.nextVideo()
+                      : null,
                   icon: const Icon(Icons.skip_next))
             ],
           ),
